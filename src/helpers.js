91442386bb6dc0d5720c24bf984282783cfc77b0
Michael Iglesias <mike.iglesias11@gmail.com>
2016-09-22 15:39:16 -0400

    no need to have processUrlParameters curried

--- src/helpers.js
+++ src/helpers.js
@@ -1,22 +1,18 @@
 import {pullAllWith as _pullAllWith, isEqual as _isEqual} from 'lodash';
-import {curry as r_curry} from 'ramda'; // eslint-disable-line camelcase
-
-function processor (selectParams, reqParams = [], funcPipeline) {
-  let requiredParamsFound = reqParams.filter((elem) => {
-    return (typeof selectParams[elem] !== 'undefined');
-  });
-
-  if (reqParams.length > 0 && requiredParamsFound.length !== reqParams.length) {
-    return Promise.reject('Not all of the required URL Parameters were found.');
-  }
-
-  return funcPipeline(selectParams);
-}
-
-let processUrlParameters = r_curry(processor);
 
 module.exports = {
-  processUrlParameters,
+  processUrlParameters (selectParams, reqParams = [], funcPipeline) {
+    let requiredParamsFound = reqParams.filter((elem) => {
+      return (typeof selectParams[elem] !== 'undefined');
+    });
+
+    if (reqParams.length > 0 && requiredParamsFound.length !== reqParams.length) {
+      return Promise.reject('Not all of the required URL Parameters were found.');
+    }
+
+    return funcPipeline(selectParams);
+  },
+
   pluckProcessedParameters (allParams, selectParams) {
     return _pullAllWith(allParams, selectParams, _isEqual);
   }
