8b762910e39b035c08d941969200701cb542fec5
Michael Iglesias <mike.iglesias11@gmail.com>
2016-09-26 15:01:26 -0400

    added default export

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import HydrationConstants from './constants';
 import HydrationActions from './actions';
 import HydrationHelpers from './helpers';
 
+module.default = HydrationReducer;
 module.exports = {
   HydrationReducer,
   HydrationConstants,
