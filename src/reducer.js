087b99834d446c0df2684b4e038f151efa14178d
Michael Iglesias <mike.iglesias11@gmail.com>
2016-09-27 11:02:32 -0400

    push redux-gatorade action through

--- src/reducer.js
+++ src/reducer.js
@@ -5,14 +5,17 @@ export default function hydrationReducer (reducer, params = []) {
 
   return function (state = INITIAL_STATE, action) {
     switch (action.type) {
-      case HydrationConstants.HYDRATE_STATE:
-        return Object.assign({}, state, {
+      case HydrationConstants.HYDRATE_STATE: {
+        let newState = Object.assign({}, state, {
           urlParameters: params.reduce((acc, currVal) => {
             acc[currVal] = action.payload[currVal];
 
             return acc;
           }, {})
         });
+
+        return reducer(newState, action);
+      }
 
       default:
         return reducer(state, action);
